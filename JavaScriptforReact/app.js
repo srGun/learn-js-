6da05151898a74582a0b028798d7493f6c31f209
Deb Endra <155624375+srGun@users.noreply.github.com>
2024-02-05 03:10:29 +0000

    update

---
+++ JavaScriptforReact/app.js
@@ -0,0 +1,13 @@
+const sum = (a ,b) =>{
+ return a+b;
+}
+let x;
+let y;
+const bitten = document.getElementsById("btn").onclick = bitten = () =>{
+x = document.getElementsById("first");
+Number(x);
+y = document.getElementsById("second");
+Number(y);
+}
+let c = sum(x,y);
+document.write(c);
